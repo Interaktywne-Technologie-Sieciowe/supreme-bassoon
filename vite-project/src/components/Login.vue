7df5373876cf52ebc647134c94efd6c07520814a
Far-ki <pat.wawrzonek@gmail.com>
2025-03-26 14:54:34 +0100

    Send in JSON email/haslo

--- vite-project/src/components/Login.vue
+++ vite-project/src/components/Login.vue
@@ -53,8 +53,20 @@
   const email = ref('')
   const password = ref('')
   
-  const login = () => {
-    console.log('Logowanie:', email.value, password.value)
+  const login = async () => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(password.value)
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data)
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
+
+  const dane = {
+    email: email.value,
+    password: hashHex,
   }
+
+  console.log('JSON:', JSON.stringify(dane))
+}
+
   </script>
   
