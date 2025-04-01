d57d845584e62dbb9ae6a71b1167ed407eb434f4
Joanna Koczyńska <110031257+joanna-koczynska@users.noreply.github.com>
2025-04-01 12:58:01 +0200

    Update insert_data.sql

--- database/insert_data.sql
+++ database/insert_data.sql
@@ -2,7 +2,21 @@
 INSERT INTO USERS_ROLES (name,code) 
 VALUES ('guest','g');
 
+INSERT INTO USERS_ROLES (name,code) 
+VALUES ('admin','a');
+
+INSERT INTO USERS_ROLES (name,code) 
+VALUES ('user','u');
+
 -- USERS
-INSERT INTO USERS (name, surname, phone, email, password, role_id) 
-VALUES ('Jan', 'Kowalski', '123456789', 'jankowalski@mail.com', 'jkowalski',
-       (SELECT id FROM USERS_ROLES WHERE name = 'guest' LIMIT 1));
+INSERT INTO USERS (id, name, surname, phone, email, password, created_at, last_login, role_id) VALUES
+(gen_random_uuid(), 'Jan', 'Kowalski', '123456789', 'jan.kowalski@mail.com', 'jkowalski', now(), NULL, (SELECT id FROM USERS_ROLES WHERE code = 'a' LIMIT 1)),
+(gen_random_uuid(), 'Ewa', 'Nowak', '987654321', 'ewa.nowak@mail.com', 'enowak', now(), NULL, (SELECT id FROM USERS_ROLES WHERE code = 'u' LIMIT 1)),
+(gen_random_uuid(), 'Jan', 'Wiśniewski', '111222333', 'jan.wisniewski@mail.com', 'jwisniewski', now(), NULL, (SELECT id FROM USERS_ROLES WHERE code = 'u' LIMIT 1)),
+(gen_random_uuid(), 'Maria', 'Dąbrowska', '444555666', 'maria.dabrowska@mail.com', 'mdebrowska', now(), NULL, (SELECT id FROM USERS_ROLES WHERE code = 'u' LIMIT 1)),
+(gen_random_uuid(), 'Piotr', 'Lewandowski', '777888999', 'piotr.lewandowski@mail.com', 'plewandowski', now(), NULL, (SELECT id FROM USERS_ROLES WHERE code = 'u' LIMIT 1)),
+(gen_random_uuid(), 'Anna', 'Zielińska', '333222111', 'anna.zielinska@mail.com', 'azielinska', now(), NULL, (SELECT id FROM USERS_ROLES WHERE code = 'g' LIMIT 1)),
+(gen_random_uuid(), 'Tomasz', 'Szymański', '666777888', 'tomasz.szymanski@mail.com', 'tszymanowski', now(), NULL, (SELECT id FROM USERS_ROLES WHERE code = 'g' LIMIT 1)),
+(gen_random_uuid(), 'Karolina', 'Wójcik', '999000111', 'karolina.wojcik@mail.com', 'kwojcik', now(), NULL, (SELECT id FROM USERS_ROLES WHERE code = 'g' LIMIT 1)),
+(gen_random_uuid(), 'Marek', 'Kozłowski', '222333444', 'marek.kozlowski@mail.com', 'mkozlowski', now(), NULL, (SELECT id FROM USERS_ROLES WHERE code = 'g' LIMIT 1)),
+(gen_random_uuid(), 'Katarzyna', 'Jankowska', '555666777', 'katarzyna.jankowska@mail.com', 'kjankiewska', now(), NULL, (SELECT id FROM USERS_ROLES WHERE code = 'g' LIMIT 1));
