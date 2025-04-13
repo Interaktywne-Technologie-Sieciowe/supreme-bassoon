66e089799056fc67b97acd35611fd7bf64827862
MateuszS <mateuszxxxsobczak@gmail.com>
2025-04-14 00:20:17 +0200

    small cleanup

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { login, sendEmail } = require('../controllers/authController');
+const { login } = require('../controllers/authController');
 
 router.post('/', login);
-router.post('/send-email', sendEmail);
 
 module.exports = router;
