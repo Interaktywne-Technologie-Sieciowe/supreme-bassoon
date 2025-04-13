aa6aa0165c773ef82b3e1a710e053a677fb59419
baranekborzy <bspsp12@gmail.com>
2025-04-13 20:12:33 +0200

    handle calendar events from backend API

--- vite-project/src/components/EventCalendar.vue
+++ vite-project/src/components/EventCalendar.vue
@@ -1,15 +1,46 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import plLocale from '@fullcalendar/core/locales/pl'
 
-const calendarOptions = {
+const events = ref([])
+
+const fetchEvents = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/api/events')
+    const data = await response.json()
+
+    events.value = data.map((event: any) => ({
+      id: event.id,
+      title: `${event.name} (${event.conference_name})`,
+      start: event.start_date,
+      end: event.end_date,
+      extendedProps: {
+        location: event.location,
+        conferenceId: event.conference_id,
+      },
+      backgroundColor: '#7c3aed', 
+      borderColor: '#6d28d9',
+      textColor: 'white',
+    }))
+  } catch (error) {
+    console.error('Błąd podczas pobierania eventów:', error)
+  }
+}
+
+onMounted(() => {
+  fetchEvents()
+})
+
+const calendarOptions = ref({
   plugins: [dayGridPlugin, interactionPlugin],
   initialView: 'dayGridMonth',
   height: '100%',
-  locale: plLocale
-}
+  locale: plLocale,
+  events: events
+})
 </script>
 
 <template>
@@ -39,10 +70,10 @@ const calendarOptions = {
 }
 
 :deep(.fc-next-button) {
-    margin-right: 30px;
+  margin-right: 30px;
 }
 
 :deep(#fc-dom-1) {
-    margin-left: 30px;
+  margin-left: 30px;
 }
 </style>
