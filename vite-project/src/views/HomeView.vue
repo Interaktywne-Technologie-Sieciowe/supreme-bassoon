879e8f2e1e9714011eed106cdad297a191832a5e
Sebastian Sadowy <sebasadowy@gmail.com>
2025-04-19 21:09:54 +0200

    add spinner on login

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import Login from '../components/UserLogin.vue'
+import CalendarView from './CalendarView.vue' // lub odpowiednia ścieżka
+import { useAuthStore } from '@/stores/auth'
+import { computed } from 'vue'
+
+const auth = useAuthStore()
+
+const isLoggedIn = computed(() => !!auth.user)
 </script>
 
 <template>
   <main>
-    <Login />
+    <!-- Jeśli nie zalogowany, pokaż login -->
+    <Login v-if="!isLoggedIn" />
+
+    <!-- Po zalogowaniu pokaż kalendarz -->
+    <CalendarView v-else />
   </main>
 </template>
