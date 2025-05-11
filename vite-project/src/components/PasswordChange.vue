e047d945c95e5158dc5173987a0c0dc2033afe0d
Joanna Koczyńska <joannakoczynska@gmail.com>
2025-05-11 19:11:29 +0200

    added div second password

--- vite-project/src/components/PasswordChange.vue
+++ vite-project/src/components/PasswordChange.vue
@@ -14,6 +14,13 @@
             placeholder="hasło" />
         </div>
 
+        <div class="space-y-2">
+          <label for="password" class="block text-sm font-medium">Potwierdź hasło</label>
+          <input id="secPassword" v-model="password" type="password" required
+            class="w-full px-4 py-2 bg-white/20 text-white placeholder-white/70 rounded-xl border border-white/30 focus:outline-none focus:ring-2 focus:ring-white"
+            placeholder="hasło" />
+        </div>
+
         <button type="submit"
           class="w-full py-2 px-4 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-xl transition">
           Zatwierdź zmianę hasła
@@ -76,4 +83,6 @@ const submitReset = async () => {
     error.value = err.message || 'Wystąpił błąd przy zmianie hasła.';
   }
 };
+
+
 </script>
